d3001b7940aba8166eb4e035a916acd794bf3aca
Allan Thraen <allan@codeart.dk>
2021-06-13 18:33:07 +0200

    Updated with 2 new commands

---
+++ src/CodeArt.Episerver.Console/Commands/AddToProjectCommand.cs
@@ -0,0 +1,78 @@
+﻿using CodeArt.Episerver.DevConsole.Attributes;
+using CodeArt.Episerver.DevConsole.Interfaces;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeArt.Episerver.Console.Commands
+{
+    [Command(Keyword = "addtoproject")]
+    public class AddToProjectCommand : IConsoleCommand, IInputCommand
+    {
+        private readonly ProjectRepository projectRepository;
+        private readonly IContentRepository contentRepository;
+
+        [CommandParameter]
+        public string ProjectName { get; set; }
+
+        [CommandParameter]
+        public string Content { get; set; }
+
+
+        private Project project;
+
+        public AddToProjectCommand(ProjectRepository projectRepo, IContentRepository contentRepository)
+        {
+            projectRepository = projectRepo;
+            this.contentRepository = contentRepository;
+        }
+
+        public string Execute(params string[] parameters)
+        {
+            if (project == null)
+            {
+                if (string.IsNullOrEmpty(ProjectName) && parameters.Length > 0)
+                {
+                    ProjectName = parameters.First();
+                }
+                var projects = projectRepository.List();
+                project = projects.Where(p => p.Name == ProjectName).FirstOrDefault();
+            }
+            if (!string.IsNullOrEmpty(Content))
+            {
+                var c = contentRepository.Get<IContent>(ContentReference.Parse(Content));
+                projectRepository.SaveItems(new ProjectItem[] { new ProjectItem(project.ID, c) });
+            }
+            //Find project
+
+            //TODO: Check if items already exist in project - if so don't add
+
+
+            return "Completed Add to Project";
+        }
+
+        public void Initialize(IOutputCommand Source, params string[] parameters)
+        {
+            Source.OnCommandOutput += Source_OnCommandOutput;
+            if (string.IsNullOrEmpty(ProjectName) && parameters.Length > 0)
+            {
+                ProjectName = parameters.First();
+            }
+            var projects=projectRepository.List();
+            project = projects.Where(p => p.Name == ProjectName).FirstOrDefault();
+        }
+
+        private void Source_OnCommandOutput(IOutputCommand sender, object output)
+        {
+            //input is IContent? Add to project
+            if(output is IContent)
+            {
+                projectRepository.SaveItems(new ProjectItem[] { new ProjectItem(project.ID, output as IContent) });
+            }
+        }
+    }
+}
---
+++ src/CodeArt.Episerver.Console/Commands/ListContentFromPropertiesCommand.cs
@@ -0,0 +1,80 @@
+﻿using CodeArt.Episerver.DevConsole.Attributes;
+using CodeArt.Episerver.DevConsole.Interfaces;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddToProjectSite.AddToProject
+{
+    [Command(Keyword = "listcontentfromproperties", Description = "Get dependent content types")]
+    public class GetDependentsCommand : IConsoleCommand, IOutputCommand
+    {
+        [CommandParameter]
+        public string Parent{ get; set; }
+
+        public event CommandOutput OnCommandOutput;
+
+        private readonly IContentSoftLinkRepository contentSoftLinkRepository;
+
+        private readonly IContentRepository contentRepository;
+
+        //Analyze recursive!!
+        private IEnumerable<IContent> ListChildElements(IContent c)
+        {
+            foreach(var p in c.Property.Where(pp => !pp.Name.StartsWith("Page")))
+            {
+                if (p.Type == PropertyDataType.Block)
+                {
+                    //contentRepository.Get<IContent>()
+                } else if (p.Type == PropertyDataType.ContentReference)
+                {
+                    yield return contentRepository.Get<IContent>((ContentReference)p.Value);
+                }
+                else if(p.Value is ContentArea) //ContentArea, XHTML, ??
+                {
+                    var lst=(p.Value as ContentArea).Items.Select(i => contentRepository.Get<IContent>(i.ContentLink));
+                    //Should we recursively handle block in blocks?
+                    foreach (var l in lst) yield return l;
+
+                } else if(p.Value is XhtmlString)
+                {
+                    //TODO, handle
+                }
+
+            }
+            //Nothing to return
+        }
+
+        public GetDependentsCommand(IContentSoftLinkRepository contentSoftLinkRepository, IContentRepository contentRepository)
+        {
+            this.contentSoftLinkRepository = contentSoftLinkRepository;
+            this.contentRepository = contentRepository;
+        }
+        public string Execute(params string[] parameters)
+        {
+            int cnt = 0;
+
+            if (string.IsNullOrEmpty(Parent)) Parent = parameters.First();
+
+            ContentReference cr = ContentReference.Parse(Parent);
+
+            var lst=ListChildElements(contentRepository.Get<IContent>(cr)).Distinct().ToList();
+
+            cnt = lst.Count();
+
+            foreach (var l in lst) OnCommandOutput.Invoke(this, l);
+            //var lst =contentSoftLinkRepository.Load(cr).Where(cs => cs.OwnerContentLink.CompareToIgnoreWorkID(cr));
+            //foreach (var l in lst.Select(ll => ll.ReferencedContentLink))
+            //{
+            //    cnt++;
+            //    OnCommandOutput.Invoke(this, contentRepository.Get<IContent>(l));
+            //}
+
+            return $"Found {cnt} dependents";
+        }
+    }
+}
